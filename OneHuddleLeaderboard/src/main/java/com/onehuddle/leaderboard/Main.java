806578dd57d58381b98568fba2cd6ac16fb2019a
ragha <ragha@raga-mac-book-pro-2017.local>
2018-02-27 15:54:41 +0530

    Added Sub Projects

---
+++ OneHuddleLeaderboard/src/main/java/com/onehuddle/leaderboard/Main.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.onehuddle.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.onehuddle.leaderboard.redis.EventSubscriber;
+import com.onehuddle.leaderboard.redis.Leaderboard;
+
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+public class Main {
+	
+    public static void main(String[] args) throws Exception {    	
+    		Object[] sources = {Main.class, LeaderBoard.class};
+    		ApplicationContext applicationContext =  SpringApplication.run(sources, args);        
+        Leaderboard.DEFAULT_REDIS_HOST = args[0];
+        	Leaderboard.DEFAULT_REDIS_PORT = Integer.valueOf(args[1]);        	
+        	Thread subscriber = new Thread(applicationContext.getBean(EventSubscriber.class));        	
+        	subscriber.start();
+        	
+    }
+
+}
